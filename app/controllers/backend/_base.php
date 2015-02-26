6546a3b6eb4ca3488bb2aa31f53f08ec90f8c07c
Michael Chan <waifung0207@gmail.com>
2015-02-26 20:42:25 +0800

    Add menu variable for frontend and backend

--- app/controllers/backend/_base.php
+++ app/controllers/backend/_base.php
@@ -9,6 +9,38 @@ abstract class _Base extends \controllers\_Base {
 
 	public function beforeRoute() {
 		$this->f3->set('site', 'backend');
+
+		$this->f3->set('menu', array(
+			array(
+				'name'		=> 'Home',
+				'url'		=> '',
+				'icon'		=> 'fa fa-home',
+			),
+			array(
+				'name'		=> 'Example',
+				'url'		=> 'example',
+				'icon'		=> 'fa fa-cogs',
+				'children'	=> array(
+					array(
+						'name'	=> 'Example 1',
+						'url'	=> 'example/1',
+					),
+					array(
+						'name'	=> 'Example 2',
+						'url'	=> 'example/2',
+					),
+					array(
+						'name'	=> 'Example 3',
+						'url'	=> 'example/3',
+					),
+				),
+			),
+			array(
+				'name'		=> 'Logout',
+				'url'		=> 'logout',
+				'icon'		=> 'fa fa-sign-out',
+			),
+		));
 	}
 
 	public function afterRoute() {
--- app/controllers/frontend/_base.php
+++ app/controllers/frontend/_base.php
@@ -9,6 +9,35 @@ abstract class _Base extends \controllers\_Base {
 
 	public function beforeRoute() {
 		$this->f3->set('site', 'frontend');
+
+		$this->f3->set('menu', array(
+			array(
+				'name'		=> 'Home',
+				'url'		=> '',
+			),
+			array(
+				'name'		=> 'Example',
+				'url'		=> 'example',
+				'children'	=> array(
+					array(
+						'name'	=> 'Example 1',
+						'url'	=> 'example/1',
+					),
+					array(
+						'name'	=> 'Example 2',
+						'url'	=> 'example/2',
+					),
+					array(
+						'name'	=> 'Example 3',
+						'url'	=> 'example/3',
+					),
+				),
+			),
+			array(
+				'name'		=> 'About',
+				'url'		=> 'about',
+			),
+		));
 	}
 
 	public function afterRoute() {
