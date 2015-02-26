4e279486d86a24392e7dd3eb0d6c38fd508d2c7a
Michael Chan <waifung0207@gmail.com>
2015-02-26 18:56:08 +0800

    Put backend skin value to config

--- app/controllers/backend/_base.php
+++ app/controllers/backend/_base.php
@@ -9,7 +9,6 @@ abstract class _Base extends \controllers\_Base {
 
 	public function beforeRoute() {
 		$this->f3->set('site', 'backend');
-		$this->f3->set('skin', 'skin-blue');
 	}
 
 	public function afterRoute() {
